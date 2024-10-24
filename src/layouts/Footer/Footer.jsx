8aebbff277885770824b1c80593301184a53e57b
AterixGz <tpsbb123@gmail.com>
2024-10-24 22:24:45 +0700

    Upload

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="Footer-container">
+      <h2>
+        <span className="badge bg-info">SPU</span>
+        &nbsp;&minus;&nbsp;
+        <span className="badge bg-info">SIT</span>
+        &nbsp;&minus;&nbsp;
+        <span className="badge bg-info">CSI</span>
+      </h2>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home-container">
+      <div className="Home-img">
+        <img src="../public/image.jpg" alt="" />
+      </div>
+      <div className="Home-text">
+        <h1>
+          Hi, I'm <span>Nemo</span>
+        </h1>
+        <h4>
+          &nbsp;&nbsp;สาขาวิชาวิทยาการคอมพิวเตอร์และนวัตกรรมการพัฒนาซอฟต์แวร์
+        </h4>
+        <p>
+          &nbsp;&nbsp;"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis facilis pariatur non, incidunt iste quia tempora. Incidunt ipsam ex distinctio aliquam modi quam vel expedita, sit non molestiae dicta cumque accusamus at asperiores sed veritatis, reprehenderit nisi iste ab deleniti labore voluptatibus. Adipisci, facere? Voluptates doloribus tenetur numquam tempora quos itaque incidunt saepe facilis explicabo expedita rem quod porro voluptate, quas dolore quis dolorum amet maxime adipisci excepturi. Culpa reprehenderit atque porro. Modi quae accusantium voluptatum. Molestias quae dolore provident illum voluptatum, accusamus id a iste, mollitia in assumenda deleniti ut repellendus sapiente laborum? Et minus animi eaque facilis accusamus."
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
